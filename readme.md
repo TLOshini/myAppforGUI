d6ddbbcb21e4e8964994fb5482777bc430b7fa62
TLOshini <oshini.goonetilleke@rmit.edu.au>
2014-03-04 16:45:02 +1100

    Inital commit

---
+++ readme.md
@@ -0,0 +1,5 @@
+#Read me file is like the Wiki to your GIt project
+You can write details of your project here
+
+
+Piled higher and deeper
---
+++ src/myappforgui/MyAppforGUI.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package myappforgui;
+
+/**
+ *
+ * @author e10505
+ */
+public class MyAppforGUI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello everyone here I am");
+        System.out.println("Lets write another program");
+    }
+    
+}
